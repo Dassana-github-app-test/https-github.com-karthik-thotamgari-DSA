6a57d666d1dcedfd4342f5ef64c83b363803d678
karthik reddy <86413998+karthik-thotamgari@users.noreply.github.com>
2022-07-22 23:30:55 +0530

    Time: 10 ms (27.37%), Space: 51.9 MB (7.13%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.java
@@ -0,0 +1,40 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] arr, int target) {
+        List<List<Integer>> temp = new ArrayList<>();
+         List<Integer> ar = new ArrayList<>();
+        Arrays.sort(arr);
+        return generatesum(arr,target,0,ar);
+    }
+    public  List<List<Integer>> generatesum(int[] arr,int target, int i,List<Integer> ar) {
+         if(i==arr.length){
+            if(target==0){
+                List<List<Integer>> temp = new ArrayList<>();
+                temp.add(new ArrayList<>(ar));
+                return temp;
+            }
+            List<List<Integer>> temp = new ArrayList<>();
+                return temp;
+        }
+        if(target==0){
+            List<List<Integer>> temp = new ArrayList<>();
+            temp.add(new ArrayList<>(ar));
+            return temp;
+        }
+        if(arr[i]>target){
+            List<List<Integer>> temp = new ArrayList<>();
+            return temp;
+        }
+        ar.add(arr[i]);
+        List<List<Integer>> with = generatesum(arr,target-arr[i],i,ar);
+        ar.remove(ar.size()-1);
+        List<List<Integer>> without = generatesum(arr,target,i+1,ar);
+        List<List<Integer>> temp = new ArrayList<>();
+        for(List<Integer> i1:with){
+            temp.add(new ArrayList<>(i1));
+        }
+        for(List<Integer> i1:without){
+            temp.add(new ArrayList<>(i1));
+        }
+        return temp;
+    }
+}

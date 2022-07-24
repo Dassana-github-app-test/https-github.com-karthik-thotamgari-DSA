5e1cbfa164a8fe46623e662318674e354be139c1
karthik reddy <86413998+karthik-thotamgari@users.noreply.github.com>
2022-07-24 19:07:12 +0530

    Time: 12 ms (12.59%), Space: 44 MB (50.07%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.java
+++ 40-combination-sum-ii/40-combination-sum-ii.java
@@ -11,9 +11,9 @@ void dfs_com(int[] cand, int cur, int target, List<Integer> path, List<List<Inte
         res.add(new ArrayList(path));
         return ;
     }
-    if (target < 0) return;
     for (int i = cur; i < cand.length; i++){
         if (i > cur && cand[i] == cand[i-1]) continue;
+        if (target < 0) break;
         path.add(cand[i]);
         dfs_com(cand, i+1, target - cand[i], path, res);
         path.remove(path.size()-1);
